8f60633ff380acc2e3590e369deeb16017afd6e2
Daniel Watoro <watorondirangu@gmail.com>
2024-07-15 21:07:01 +0300

    Adds new code

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({ name, email })
+    })
+    .then(response => response.json())
+    .then(({ id }) => {
+      document.body.innerHTML = id;
+    })
+    .catch(error => {
+      document.body.innerHTML = error.message;
+    });
+  }
